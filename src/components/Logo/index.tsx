5206e539a761f3eebb991bf92316d896e05847de
fetz <emmsel@on.sinch.com>
2022-11-11 10:28:53 +0100

    formatting

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,5 +1,7 @@
 import serloLogo from '../../assets/images/serlo-logo.svg';
 
-const Logo = () => <img width="164px" height="164px" src={serloLogo} />;
+const Logo = () => (
+  <img width="164px" height="164px" alt="serlo" src={serloLogo} />
+);
 
 export { Logo };
